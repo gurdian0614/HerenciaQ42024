8df9b74de90c6a26d6b975494787185c9ae703fc
gurdian0614 <ivan.gurdian@outlook.com>
2024-10-16 18:32:20 -0600

    Creando clase invertebrado

--- HerenciaQ42024/Invertebrado.cs
+++ HerenciaQ42024/Invertebrado.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace HerenciaQ42024
 {
-    internal class Invertebrado
+    public class Invertebrado : Animal
     {
+        public string TipoCuerpo { get; set; }
+        public string MetodoReproduccion { get; set; }
+        public string TipoAlimentacion { get; set; }
+        public string HabitatPreferido { get; set; }
+        public int CicloVida { get; set; }
+        public string Tamano { get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Especie: {Especie}");
+            Console.WriteLine($"Habitat: {Habitat}");
+            Console.WriteLine($"Tipo de cuerpo: {TipoCuerpo}");
+            Console.WriteLine($"Método de reproducción: {MetodoReproduccion}");
+            Console.WriteLine($"Tipo de Alimentación: {TipoAlimentacion}");
+            Console.WriteLine($"Habitat preferido: {HabitatPreferido}");
+            Console.WriteLine($"Ciclo de Vida: {CicloVida}");
+            Console.WriteLine($"Tamño: {Tamano}");
+        }
     }
 }
